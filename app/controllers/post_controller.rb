ad176602b3457bed548a77383ac2ab11b1902bea
Sheikh hamza <sheikhhamza012@gmail.com>
2019-10-10 19:25:21 +0500

    images made

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,11 +1,12 @@
 class PostController < ApplicationController
     def new
+        puts 
         @post = User.find(current_user.id).posts.new(up_params)
         if @post.save
             redirect_to('/')
         end
     end
     def up_params
-        params.require(:post).permit(:msg,:msg_type)
+        params.require(:post).permit(:msg, :msg_type, :image)
     end
 end
